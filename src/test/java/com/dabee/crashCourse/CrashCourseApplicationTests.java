8a4dde48e131ef699d2957622569621d6268a9f8
mariedabee <mariejuly70@gmail.com>
2022-11-18 14:01:44 +0100

    initial set up

---
+++ src/test/java/com/dabee/crashCourse/CrashCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dabee.crashCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrashCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
